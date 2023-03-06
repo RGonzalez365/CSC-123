9449a630a2acdf5ced2a9c73861a7bfc67ba0aa3
Raul E Gonzalez <Raulg5683@gmail.com>
2023-03-06 13:42:46 -0800

    Organized all my files and folders.

---
+++ CSC_123_1_Master/src/Hello.java
@@ -0,0 +1,12 @@
+// Title: Hello
+// Author: Raul E Gonzalez
+// E-mail: Rgonzalez365@toromail.csudh.edu
+// Date: 03/06/2023
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+}
